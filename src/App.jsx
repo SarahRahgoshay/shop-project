9d84d7461160d79b38207fb190e63639d864dbe7
sarahRahgoshay <sarah.rahgoshay@gmail.com>
2024-02-06 13:51:10 +0330

    feat: wrapping productProvider around routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import ProductsPage from "./pages/ProductsPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import PageNotFound from "./pages/PageNotFound";
+import ProductProvider from "./context/ProductContext";
 
 const App = () => {
   return (
-     <Routes>
-      <Route index element={<Navigate to="/products" />} />
-      <Route path="/products" element={<ProductsPage />} />
-      <Route path="/products/:id" element={<ProductDetailsPage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
-      <Route path="/*" element={<PageNotFound />} />
-     </Routes> 
+    <ProductProvider>
+      <Routes>
+        <Route index element={<Navigate to="/products" />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<ProductDetailsPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes> 
+    </ProductProvider>
   );
 };
 
