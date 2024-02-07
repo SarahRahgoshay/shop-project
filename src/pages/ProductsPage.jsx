8c40ee3bfa63a80281dceae334f59ad958d0b4e5
sarahRahgoshay <sarah.rahgoshay@gmail.com>
2024-02-07 19:18:38 +0330

    feat: rendering products with style

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,7 +1,21 @@
+import Card from "../components/Card";
+import Loader from "../components/Loader";
+import { useProducts } from "../context/ProductContext";
+import styles from "../styles/ProductsPage.module.css"
+
 const ProductsPage = () => {
+
+    const products = useProducts();
+
     return (
-        <div>
-            products
+        <div className={styles.container}>
+            <div className={styles.products}> 
+            {!products.length && <Loader /> }
+                {products.map((product) => (
+                    <Card key={product.id} data={product} />
+            ))}
+            </div>
+            <div>sidebar</div>
         </div>
     );
 };
