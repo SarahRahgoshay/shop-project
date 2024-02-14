1ccf6c3a5115ff4544aa76140715b42f9700c4d3
sarahRahgoshay <sarah.rahgoshay@gmail.com>
2024-02-14 21:07:40 +0330

    feat: showing product details

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,7 +1,42 @@
+import { Link, useParams } from "react-router-dom";
+import { useProductDetails } from "../context/ProductContext";
+import Loader from "../components/Loader";
+import { GiPriceTag } from "react-icons/gi";
+import { SiOpenproject } from "react-icons/si";
+import { MdArrowBack } from "react-icons/md";
+import styles from "../styles/ProductDetailsPage.module.css"
+
 const ProductDetailsPage = () => {
+    const {id} = useParams();
+
+    const productDetails = useProductDetails(+id)
+
+    if (!productDetails) return <Loader />
+
+    const {image , title , description , price , category} = productDetails;
+
+
     return (
-        <div>
-            product detail
+        <div className= {styles.container}>
+            <img src= {image} alt= {title} />
+            <div className= {styles.information}>
+                <h3 className= {styles.title}> {title} </h3>
+                <p className= {styles.description}> {description} </p>
+                <p className= {styles.category}> 
+                    <SiOpenproject />
+                    {category} 
+                </p>
+                <div>
+                    <span className= {styles.price}> 
+                        <GiPriceTag />
+                        {price} $ 
+                    </span>
+                    <Link to="/products"> 
+                        <MdArrowBack />
+                        Back to Shop
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
