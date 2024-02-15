a5c2568b5c43b906fc6e4afba4864fd28d56a37b
sarahRahgoshay <sarah.rahgoshay@gmail.com>
2024-02-15 16:40:45 +0330

    feat: edit ShoppingCart

--- src/components/ShoppingCard.jsx
+++ src/components/ShoppingCard.jsx
@@ -4,12 +4,13 @@ import styles from "../styles/ShoppingCard.module.css"
 
 const ShoppingCard = ({data , clickHandler}) => {
 
-    const { image , title , quantity} = data;
+    const { image , title , quantity , price} = data;
 
     return (
         <div className= {styles.card}>
             <img src={image} alt={title} />
             <p> {shortenText(title)} </p>
+            <p> {price} $ </p>
             <div className={styles.actions}>
                 {quantity == 1 && (<button onClick={() => clickHandler("REMOVE_ITEM" , data)}> <BsTrash /> </button>)}
                 {quantity > 1 && (<button onClick={() => clickHandler("DECREASE" , data)}> - </button>)}
